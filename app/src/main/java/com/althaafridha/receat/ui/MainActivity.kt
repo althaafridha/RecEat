6b2e17d942c687112b4f717d1cf734782ee9272c
Althaafzz <althaaf.zaydaan@gmail.com>
2022-05-30 13:01:25 +0700

    add package and uses-permision

--- app/src/main/java/com/althaafridha/receat/ui/MainActivity.kt
+++ app/src/main/java/com/althaafridha/receat/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.althaafridha.receat
+package com.althaafridha.receat.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.althaafridha.receat.R
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {

24b2a80b29e4a2af90900680594e21700e771b50
Danish <danishmaulahasbi@gmail.com>
2022-05-30 13:37:38 +0700

    replace fragment with activity

---
+++ app/src/main/java/com/althaafridha/receat/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.althaafridha.receat.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.althaafridha.receat.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/com/althaafridha/receat/ui/home/ActivityHome.kt
@@ -0,0 +1,12 @@
+package com.althaafridha.receat.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.althaafridha.receat.R
+
+class ActivityHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}

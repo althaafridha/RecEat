7a1b82bbffff240bfcc19c4456d93584c6d44858
Althaafzz <althaaf.zaydaan@gmail.com>
2022-06-02 07:23:10 +0700

    remove transparent status bar

--- app/src/main/java/com/althaafridha/receat/data/network/ApiService.kt
+++ app/src/main/java/com/althaafridha/receat/data/network/ApiService.kt
@@ -4,13 +4,16 @@ import com.althaafridha.receat.data.DetailResponse
 import com.althaafridha.receat.data.NewRecipeResponse
 import io.reactivex.rxjava3.core.Flowable
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
 	@GET("api/recipes")
 	fun getNewRecipe(): Flowable<NewRecipeResponse>
 
-	@GET("/api/recipe/key")
-	fun getDetailRecipe(): Flowable<DetailResponse>
+	@GET("/api/recipe/{key}")
+	fun getDetailRecipe(
+		@Query("key") key: String
+	): Flowable<DetailResponse>
 
 }
--- app/src/main/java/com/althaafridha/receat/ui/MainActivity.kt
+++ app/src/main/java/com/althaafridha/receat/ui/MainActivity.kt
@@ -23,13 +23,7 @@ class MainActivity : AppCompatActivity() {
 	private val binding get() = _binding as ActivityMainBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			val w: Window = window
-			w.setFlags(
-				WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-				WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-			)
-		}
+
 		super.onCreate(savedInstanceState)
 
 		_binding = ActivityMainBinding.inflate(layoutInflater)
